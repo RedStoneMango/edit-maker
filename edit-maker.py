9e2259b04440904cee9d861f377db7765630a9f4
RedStoneMango <RedStoneMango@outlook.com>
2026-07-02 17:01:04 +0200

    add graphic resizing

--- edit-maker.py
+++ edit-maker.py
@@ -2,25 +2,48 @@
 
 import argparse
 
-arg_parser = argparse.ArgumentParser("edit-maker", description="Generates TikTik edits")
+def size_type(strings):
+    if strings.lower() == "auto":
+        return (-1,-1)
+    
+    strings = strings.replace("(", "").replace(")", "")
+    tuple_int = tuple(map(int, strings.split(",")))
+    if len(tuple_int) != 2:
+        raise argparse.ArgumentTypeError("Should be of format WIDTH,HEIGHT or 'auto'")
+    if tuple_int[0] <= 0 or tuple_int[1] <= 0:
+        raise argparse.ArgumentTypeError("Should be greater than 0")
+    return tuple_int
+
+arg_parser = argparse.ArgumentParser("edit-maker", description="Generates TikTok edits")
 arg_parser.add_argument("audio", help="The audio file to use for the edit", type=str)
 arg_parser.add_argument("output", help="The output file to save the edit in", type=str)
 arg_parser.add_argument("graphics", help="The graphics to be used in the edit", type=str, nargs="+")
-arg_parser.add_argument("--beat-tightness", "-t", help="The tightness of the detected audio beat distribution around tempo", type=float, default=100, nargs='?')
+arg_parser.add_argument("--beat-tightness", "-t", help="The tightness of the detected audio beat distribution around the tempo of the audio file", type=float, default=100, required=False)
+arg_parser.add_argument("--size", "-s", help="The size of the resulting edit. This will scale all graphics to this value while respecting the aspect ratio. Using 'auto', the size if the max with/height of the graphics. Without this option, no scaling is applied", type=size_type, default=None, required=False, metavar="[WIDTH,HEIGHT|auto]")
 
 import librosa
 import magic
-import os
 from moviepy import AudioFileClip, ImageClip, VideoFileClip, concatenate_videoclips
 
 is_video_cache = {}
 
 def main():
     args = arg_parser.parse_args()
+
+    size = args.size
+    if size != None and size[0] == -1:
+        size = find_auto_size(args.graphics)
     
     audio_data = analyze_audio(args.audio, args.beat_tightness)
-    clips = build_clips(audio_data, args.graphics)
+    clips = build_clips(audio_data, args.graphics, size)
     render(clips, audio_data, args.output)
+
+def find_auto_size(graphics):
+    size = (0, 0)
+    for graphic in graphics:
+        inst = instantiate_clip(graphic, None, None)
+        size = tuple(max(a, b) for a, b in zip(size, inst.size))
+    return size
 
 def analyze_audio(audio_path, tightness):
     y, sr = librosa.load(audio_path)
@@ -29,7 +52,7 @@ def analyze_audio(audio_path, tightness):
     audio = AudioFileClip(audio_path)
     return (beats, audio.duration, audio)
 
-def build_clips(audio_data, graphics):
+def build_clips(audio_data, graphics, size):
     beats, duration, _ = audio_data
     clips = []
 
@@ -39,7 +62,8 @@ def build_clips(audio_data, graphics):
     for beat in beats:
         clips.append(instantiate_clip(
             graphics[current_graphic],
-            duration=beat - previous
+            beat - previous,
+            size
         ))
         current_graphic = (current_graphic + 1) % len(graphics)
         previous = beat
@@ -47,22 +71,32 @@ def build_clips(audio_data, graphics):
     # Tail after the last beat
     clips.append(instantiate_clip(
             graphics[current_graphic],
-            duration=duration - previous
+            duration - previous,
+            size
     ))
 
     return clips
 
-def instantiate_clip(graphic, duration):
+def instantiate_clip(graphic, duration, size):
     if is_video(graphic):
         clip = VideoFileClip(
             graphic
-        ).subclipped(0, duration)
+        )
+        if duration != None:
+            clip = clip.subclipped(0, duration)
     else:
         clip = ImageClip(
             graphic,
             duration=duration
         )
-
+    
+    if size != None:
+            aspect_ratio = clip.w / clip.h # Clip's own aspect_ratio is not consistently exposed for all codecs
+            if aspect_ratio > 1:
+                clip = clip.resized(width=size[0])
+            else:
+                clip = clip.resized(height=size[1])
+    
     return clip
 
 def is_video(file):
