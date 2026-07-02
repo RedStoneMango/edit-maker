040b1c6f63b310d2b8182b82081c3cdc3ecb6384
RedStoneMango <RedStoneMango@outlook.com>
2026-07-02 15:02:45 +0200

    update video stream to iterate through images instead of colors

--- edit-maker.py
+++ edit-maker.py
@@ -5,11 +5,12 @@ import argparse
 arg_parser = argparse.ArgumentParser("edit-maker", description="Generates TikTik edits")
 arg_parser.add_argument("audio", help="The audio file to use for the edit", type=str)
 arg_parser.add_argument("output", help="The output file to save the edit in", type=str)
+arg_parser.add_argument("graphics", help="The graphics to be used in the edit", type=str, nargs="+")
 arg_parser.add_argument("--beat-tightness", "-t", help="The tightness of the detected audio beat distribution around tempo", type=float, default=100, nargs='?')
 args = arg_parser.parse_args()
 
 import librosa
-from moviepy import AudioFileClip, ColorClip, concatenate_videoclips
+from moviepy import AudioFileClip, ImageClip, concatenate_videoclips
 
 audio_path = args.audio
 output_path = args.output
@@ -24,31 +25,29 @@ duration = audio.duration
 
 clips = []
 
-colors = [(0, 0, 255), (0, 255, 0)]
-current_color = 0
+images = args.graphics
+current_image = 0
 previous = 0
 
 for beat in beat_times:
     clips.append(
-        ColorClip(
-            size=(1080, 1920),
-            color=colors[current_color],
+        ImageClip(
+            images[current_image],
             duration=beat - previous,
         )
     )
-    current_color ^= 1
+    current_image = (current_image + 1) % len(images)
     previous = beat
 
 # Tail after the last beat
 clips.append(
-    ColorClip(
-        size=(1080, 1920),
-        color=colors[current_color],
+    ImageClip(
+        images[current_image],
         duration=duration - previous,
     )
 )
 
-video = concatenate_videoclips(clips)
+video = concatenate_videoclips(clips, method="compose")
 fvideo = video.with_audio(audio)
 
 fvideo.write_videofile(
