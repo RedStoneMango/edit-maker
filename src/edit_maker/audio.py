ad371769998e221dc87cf11b4e0ecb13d48c1529
RedStoneMango <RedStoneMango@outlook.com>
2026-07-04 11:59:01 +0200

    add pseudo-progress-bar to audio.py
    we don't have a way to log librosa's progress so we can only log the progress between different steps

--- src/edit_maker/audio.py
+++ src/edit_maker/audio.py
@@ -2,10 +2,20 @@ from .dataholders import AudioData
 
 import librosa
 from moviepy import AudioFileClip
+from proglog import default_bar_logger
 
 def analyze_audio(audio_path, tightness):
+    logger = default_bar_logger("bar")
+    logger.iter_bar(progress=range(5))
+    logger(message="Analyzing audio")
+
+    logger.bars_callback("progress", "index", 1, 0)
     y, sr = librosa.load(audio_path)
+    logger.bars_callback("progress", "index", 2, 1)
     _, beat_frames = librosa.beat.beat_track(y=y, sr=sr, tightness=tightness)
+    logger.bars_callback("progress", "index", 3, 2)
     beats = librosa.frames_to_time(beat_frames, sr=sr)
+    logger.bars_callback("progress", "index", 4, 3)
     audio = AudioFileClip(audio_path)
+    logger.bars_callback("progress", "index", 5, 4)
     return AudioData(beats=beats, duration=audio.duration, clip=audio)
