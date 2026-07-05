64790b74327ec8f1876bc9a8fdb9167184d14923
RedStoneMango <RedStoneMango@outlook.com>
2026-07-05 15:17:48 +0200

    add optional arguments explaination to README

--- README.md
+++ README.md
@@ -25,4 +25,20 @@ $ edit-maker audio.mp3 my-edit.mp4 image1.jpg image2.png animated.gif video.mp4
 
 The above creates an edit as *my-edit.mp4* using the music *audio.mp3* and featuring the contents *image1.jpg*, *image2.png*, *animated.gif* and *video.mp4*.
 
+---
+
 ## Optional Arguments
+
+The script accepts a set of optional arguments to customize your edits even further:
+
+| Name | Abbreviation | Decription | Value Format |
+| ---- | ------------ | ---------- | ------------ |
+| --help | -h | Show a help message in the terminal and exit |  |
+| --beat-tightness |  | A (decimal) number specifying the tightness of the detected audio beat distribution around the tempo of the audio file. Must be greater or equal to 0. The detected beats are used to align the hraphic clips with the audio | POSITIVE_NUMBER |
+| --size | -s | The size of the edit in pixels. This will scale all graphics to this value while respecting the aspect ratio. Without this option, the size is the max width/height of the provided graphics. To specify the fill of leftover areas, have a look at `--background-blur` | WIDTH,HEIGHT or WIDTHxHEIGHT or WIDTH×HEIGHT |
+| --background-blur | -B | Enables background blur for graphics whose size does not match the canvas size. Instead of the standard black area, the outer part of the graphic will be a blurred version of the graphic itself |  |
+| --graphic-beats | -b | The amount of audio beats a graphic should be displayed. Higher values mean that individual graphics are visible for a longer time. Default: 1 | POSITIVE_NUMBER_NOT_0 |
+| --darken | -d | The intensity of the darkening effect to be applied to every clip or 'off' to deactivate darkening. 0 means normal, 1 means all black. Default: off | POSITIVE_NUMBER or 'off' |
+| --vignette | -V | The base brightness of the vignette effect to be applied to every clip or 'off' to deactivate. Default: 0.8 | POSITIVE_NUMBER or 'off' |
+| <a id="transition-arg"></a> --transitions | -t | A list allowed transitions between graphic clips. Can be either a comma-separated list of names or a bitmask. Refer to the [transition documentation](#transitions) for name and index information | NAME,NAME,NAME... or INTEGER |
+| --seed |  | The seed to be used for random choices. When generating multiple videos with the same seed, the choices are guaranteed to be the same random sequence as long as no other parameters interfer with the sample space. Can be any text | TEXT |
--- src/edit_maker/cli.py
+++ src/edit_maker/cli.py
@@ -85,13 +85,13 @@ def parse_args():
     parser.add_argument("audio", help="The audio file to use for the edit", type=file_type)
     parser.add_argument("output", help="The output file to save the edit in", type=str) # Output is not required to exist, therefore no file_type
     parser.add_argument("graphics", help="The graphics to be used in the edit", type=file_type, nargs="+")
-    parser.add_argument("--beat-tightness", "-t", help="The tightness of the detected audio beat distribution around the tempo of the audio file. Must be greater or equal 0 and can have decimal points", type=positive_float_type, default=100, required=False)
+    parser.add_argument("--beat-tightness", help="The tightness of the detected audio beat distribution around the tempo of the audio file. Must be greater or equal 0 and can have decimal points", type=positive_float_type, default=100, required=False)
     parser.add_argument("--size", "-s", help="The size of the resulting edit in format WIDTH,HEIGHT / WIDTHxHEIGHT. This will scale all graphics to this value while respecting the aspect ratio. Without this option, the size is the max width/height of the provided graphics", type=size_type, default=None, required=False, metavar="[WIDTH,HEIGHT|WIDTHxHEIGHT]")
     parser.add_argument("--background-blur", "-B", help="Enables background blur for graphics whose size does not match the canvas size. Instead of a black area the outer part of the graphic will be a blurred version of the graphic itself", action="store_true")
     parser.add_argument("--graphic-beats", "-b", help="The amount of beats a graphic should be displayed. Default 1", required=False, default=1, type=positive_int_type)
     parser.add_argument("--darken", "-d", help="The intensity of the darkening effect to be applied to every clip or 'off' to deactivate. Default: off", required=False, default=None, type=deactivatable_positive_float_type, metavar="[DARKEN|off]")
     parser.add_argument("--vignette", "-V", help="The base brightness of the vignette effect to be applied to every clip or 'off' to deactivate. Default: 0.8", required=False, default=0.8, type=deactivatable_positive_float_type, metavar="[VIGNETTE|off]")
-    parser.add_argument("--seed", "-S", help="The seed to be used for random choises. When generating multiple videos with the same seed and same transition setting, the transitions are guaranteed to be the same random sequence", required=False, default=None, type=str)
-    parser.add_argument("--transitions", "-T", help="The allowed transitions between graphic clips. Can be either a comma-seperated list of names or a bitmask. Refer to documentation for name and index information", required=False, default=all_transitions(), type=transition_list_type)
+    parser.add_argument("--transitions", "-t", help="The allowed transitions between graphic clips. Can be either a comma-separated list of names or a bitmask. Refer to documentation for name and index information", required=False, default=all_transitions(), type=transition_list_type)
+    parser.add_argument("--seed", "", help="The seed to be used for random choices. When generating multiple videos with the same seed and same transition setting, the transitions are guaranteed to be the same random sequence. Can be any text", required=False, default=None, type=str)
     
     return parser.parse_args()
